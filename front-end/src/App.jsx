470e3f7c348d3bf04f318ca205e2500bd20c1f70
Se Jun Lim <lsj1137@naver.com>
2025-01-26 20:08:14 +0900

    [FD-156] chore: 프로젝트 baseCSS 세팅 완료

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,6 @@ function App() {
     <>
       <div className=' mx-5'>
       {/* IntelliSense for CSS class names in HTML 설치 필요 */}
-        <h1 className='header-1 bg-blue-300'>header 1</h1>
-        <h2 className='header-2 bg-pink-100'>header 2</h2>
-        <h3 className='header-3'>header 3</h3>
-        <div className=" w-24 h-24 bg-lime-900 rounded-400"></div>
-        <div className=" w-statusBar h-statusBar bg-gray-900 rounded-400"></div>
-        <div className=" w-statusBar h-navBar bg-amber-500 rounded-400"></div>
       </div>
     </>
   );
