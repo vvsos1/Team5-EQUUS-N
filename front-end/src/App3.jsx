f97d7d7ebfdb5d00e0a65c27a2b216d888a598a5
Se Jun Lim <lsj1137@naver.com>
2025-01-28 12:40:05 +0900

    [FD-188] chore: app3.js (테스트 화면) 수정

--- front-end/src/App3.jsx
+++ front-end/src/App3.jsx
@@ -1,11 +1,91 @@
-import Icon from './components/Icon';
+import { useState } from 'react';
 import FeedBackButton from './components/buttons/FeedBackButton';
+import LargeButton from './components/buttons/LargeButton';
+import MediumButton from './components/buttons/MediumButton';
+import SmallButton from './components/buttons/SmallButton';
 
 function App3() {
+  const [currentFeedback, setCurrentFeedback] = useState(null);
+
   return (
     <>
-      <div className='mx-5 flex h-screen items-center justify-center bg-gray-900'>
-        <FeedBackButton />
+      <div className='mx-5 flex h-full flex-col items-center justify-center gap-5 bg-gray-400'>
+        <LargeButton
+          text='큰 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={false}
+        />
+        <LargeButton
+          text='큰 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={false}
+        />
+        <LargeButton
+          text='큰 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={true}
+        />
+        <LargeButton
+          text='큰 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={true}
+        />
+        <MediumButton
+          text='중간 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={false}
+        />
+        <MediumButton
+          text='중간 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={false}
+        />
+        <MediumButton
+          text='중간 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={true}
+        />
+        <MediumButton
+          text='중간 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={true}
+        />
+        <SmallButton
+          text='작은 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={false}
+        />
+        <SmallButton
+          text='작은 버튼'
+          onClick={() => {}}
+          isOutlined={false}
+          disabled={true}
+        />
+        <SmallButton
+          text='작은 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={false}
+        />
+        <SmallButton
+          text='작은 버튼'
+          onClick={() => {}}
+          isOutlined={true}
+          disabled={true}
+        />
+        <FeedBackButton
+          currentFeedback={currentFeedback}
+          onClick={setCurrentFeedback}
+        />
       </div>
     </>
   );
