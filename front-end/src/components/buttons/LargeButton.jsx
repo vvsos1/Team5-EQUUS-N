555d07534d8c4082c779dd94dca74fa34ed1b155
Se Jun Lim <lsj1137@naver.com>
2025-01-28 12:37:22 +0900

    [FD-188] refactor: disabled 속성 반영, h-[npx], w-[npx] 속성 반영

--- front-end/src/components/buttons/LargeButton.jsx
+++ front-end/src/components/buttons/LargeButton.jsx
@@ -9,18 +9,16 @@ export default function LargeButton({
 }) {
   return (
     <button
+      disabled={disabled}
       className={classNames(
-        'rounded-300 flex w-full items-center justify-center px-4 py-2 text-gray-300',
+        'rounded-300 flex h-[56px] w-full items-center justify-center px-4 py-2 text-gray-300',
         isOutlined ?
           disabled ? 'border border-gray-500 text-gray-500'
           : 'cursor-pointer border border-lime-500 text-lime-500'
         : disabled ? 'bg-gray-800 text-gray-300'
         : 'cursor-pointer bg-lime-500 text-gray-900',
       )}
-      onClick={disabled ? () => {} : onClick}
-      style={{
-        height: '56px',
-      }}
+      onClick={onClick}
     >
       {text ?
         text
--- front-end/src/components/buttons/SmallButton.jsx
+++ front-end/src/components/buttons/SmallButton.jsx
@@ -9,19 +9,16 @@ export default function SmallButton({
 }) {
   return (
     <button
+      disabled={disabled}
       className={classNames(
-        'rounded-300 flex items-center justify-center px-4 py-2 text-gray-300',
+        'rounded-300 flex h-[46px] w-[115px] items-center justify-center px-4 py-2 text-gray-300',
         isOutlined ?
           disabled ? 'border border-gray-500 text-gray-500'
           : 'cursor-pointer border border-lime-500 text-lime-500'
         : disabled ? 'bg-gray-800 text-gray-300'
         : 'cursor-pointer bg-lime-500 text-gray-900',
       )}
-      onClick={disabled ? () => {} : onClick}
-      style={{
-        width: '115px',
-        height: '46px',
-      }}
+      onClick={onClick}
     >
       {text}
     </button>
