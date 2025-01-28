619b14911963b6c4c7fd502be6bfa9c874985580
Se Jun Lim <lsj1137@naver.com>
2025-01-28 14:17:36 +0900

    [FD-198] chore: 아이콘 크기 변하도록 고정값 삭제, 색상 변하도록 고정값 삭제

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,7 @@
-import NavBar2 from './components/NavBar2';
-
 function App() {
   return (
     <div className='mx-5 h-full w-2xl bg-gray-900'>
-      <div className='flex h-full flex-col items-center justify-center gap-4 overflow-auto'>
-        <NavBar2
-          title='피드백 주고받기'
-          canPop={true}
-          canClose={true}
-          isCloseLeft={false}
-        />
-      </div>
+      <div className='flex h-full flex-col items-center justify-center gap-4 overflow-auto'></div>
     </div>
   );
 }
