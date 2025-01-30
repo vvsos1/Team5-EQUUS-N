e39e6638114255beb1597de62d28718d03ca537c
Se Jun Lim <lsj1137@naver.com>
2025-01-30 20:45:51 +0900

    [FD-202] feat: 버튼 사이즈 수정

--- front-end/src/components/buttons/SmallButton.jsx
+++ front-end/src/components/buttons/SmallButton.jsx
@@ -11,12 +11,12 @@ export default function SmallButton({
     <button
       disabled={disabled}
       className={classNames(
-        'rounded-300 flex h-[46px] w-[115px] items-center justify-center px-4 py-2 text-gray-300',
+        'rounded-300 flex h-[52px] w-[115px] items-center justify-center text-gray-300',
         isOutlined ?
           disabled ? 'border border-gray-500 text-gray-500'
-          : 'cursor-pointer border border-lime-500 text-lime-500'
+          : 'border border-lime-500 text-lime-500'
         : disabled ? 'bg-gray-800 text-gray-300'
-        : 'cursor-pointer bg-lime-500 text-gray-900',
+        : 'bg-lime-500 text-gray-900',
       )}
       onClick={onClick}
     >
