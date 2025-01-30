a3ea5a763d7443e00714b9e82812914fd0b11d97
GoGradually <chkun3109@gmail.com>
2025-01-30 17:23:51 +0900

    [FD-12] feat: 로그인 검증 인터셉터 등록

---
+++ back-end/src/main/java/com/feedhanjum/back_end/auth/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.feedhanjum.back_end.auth.config;
+
+import com.feedhanjum.back_end.auth.infra.AuthInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor())
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/**");
+    }
+
+    @Bean
+    public AuthInterceptor authInterceptor() {
+        return new AuthInterceptor();
+    }
+}
