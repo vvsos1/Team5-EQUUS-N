b243f5fcfd6757047b148058bb10763cd72a457a
vvsos1 <vvsos10@naver.com>
2025-02-01 22:11:26 +0900

    [FD-34] feat: 일정 종료 이벤트 발생과 정기 피드백 요청 생성을 연결시키는 이벤트 리스너 추가

---
+++ back-end/src/main/java/com/feedhanjum/back_end/event/config/EventConfig.java
@@ -0,0 +1,9 @@
+package com.feedhanjum.back_end.event.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class EventConfig {
+}
