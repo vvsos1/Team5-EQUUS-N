a5066d38cde22172c5b9ebbf7ac1c423a5230796
vvsos1 <vvsos10@naver.com>
2025-02-01 12:23:11 +0900

    [FD-241] refactor: 팀장 정보 팀 도메인으로 이동

--- back-end/src/main/java/com/feedhanjum/back_end/team/domain/Team.java
+++ back-end/src/main/java/com/feedhanjum/back_end/team/domain/Team.java
@@ -1,6 +1,7 @@
 package com.feedhanjum.back_end.team.domain;
 
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
+import com.feedhanjum.back_end.member.domain.Member;
 import com.feedhanjum.back_end.schedule.domain.Schedule;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -35,10 +36,15 @@ public class Team {
     @OneToMany(mappedBy = "team")
     private final List<Schedule> schedules = new ArrayList<>();
 
-    public Team(FeedbackType feedbackType, String name, LocalDateTime startTime, LocalDateTime endTime) {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "leader_id", nullable = false)
+    private Member leader;
+
+    public Team(String name, Member leader, LocalDateTime startTime, LocalDateTime endTime, FeedbackType feedbackType) {
         this.feedbackType = feedbackType;
         this.name = name;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.leader = leader;
     }
 }
