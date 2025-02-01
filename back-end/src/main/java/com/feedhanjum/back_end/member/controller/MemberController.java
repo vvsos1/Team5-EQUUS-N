812f264cc55ad715edde51795bc0170fee71484c
GoGradually <chkun3109@gmail.com>
2025-02-01 18:14:59 +0900

    [FD-8] feat: 회원 이름 변경 컨트롤러 구현

--- back-end/src/main/java/com/feedhanjum/back_end/member/controller/MemberController.java
+++ back-end/src/main/java/com/feedhanjum/back_end/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.feedhanjum.back_end.member.controller;
 
+import com.feedhanjum.back_end.auth.infra.Login;
 import com.feedhanjum.back_end.member.controller.dto.MemberDto;
 import com.feedhanjum.back_end.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,12 @@ public class MemberController {
     @GetMapping("/member/{id}")
     public ResponseEntity<MemberDto> getMemberById(@PathVariable Long id) {
         MemberDto memberDto = new MemberDto(memberService.getMemberById(id));
+        return new ResponseEntity<>(memberDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/member/name")
+    public ResponseEntity<MemberDto> changeName(@Login Long memberId, String name) {
+        MemberDto memberDto = new MemberDto(memberService.changeName(memberId, name));
         return new ResponseEntity<>(memberDto, HttpStatus.OK);
     }
 }
