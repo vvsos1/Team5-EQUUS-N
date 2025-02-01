3a3c777248823b617ecf72d4311776d8d14b6f70
vvsos1 <vvsos10@naver.com>
2025-02-01 22:12:42 +0900

    [FD-34] feat: 일정 리포지터리에 멤버도 함께 fetch 해오는 메서드 추가

---
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,13 @@
+package com.feedhanjum.back_end.schedule.repository;
+
+import com.feedhanjum.back_end.schedule.domain.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("select s from Schedule s join fetch s.scheduleMembers m join fetch m.member where s.id = :id")
+    Optional<Schedule> findByIdWithMembers(Long id);
+}
