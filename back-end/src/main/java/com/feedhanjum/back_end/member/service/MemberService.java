beb8d1627b6bfa1d45d1c5bf16173956ffccb234
GoGradually <chkun3109@gmail.com>
2025-02-01 17:54:32 +0900

    [FD-8] feat: MemberService에 회원 활동명 변경 추가

--- back-end/src/main/java/com/feedhanjum/back_end/member/service/MemberService.java
+++ back-end/src/main/java/com/feedhanjum/back_end/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.feedhanjum.back_end.member.service;
 
+import com.feedhanjum.back_end.auth.infra.Login;
 import com.feedhanjum.back_end.member.domain.Member;
 import com.feedhanjum.back_end.member.repository.MemberRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -20,5 +21,13 @@ public class MemberService {
         Member findMember = memberRepository.findById(findMemberId).orElseThrow(() -> new EntityNotFoundException("찾으려는 해당 사용자가 없습니다."));
         // 나중에 soft delete 구현 시, 탈퇴한 회원 여부를 검사해야 함.
         return findMember;
+    }
+
+    @Transactional
+    public Member changeName(@Login Long memberId, String name) {
+        // 이거 이럴 일이 없겠지만, 동시성 문제 발생할지 모르니 혹시 몰라서 남겨둡니다.
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RuntimeException("DB 오류"));
+        loginMember.changeName(name);
+        return loginMember;
     }
 }
