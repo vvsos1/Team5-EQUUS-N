bfd3b22de78269b19318af96d82ee07df20d0c17
GoGradually <chkun3109@gmail.com>
2025-02-02 18:16:56 +0900

    [FD-13] feat: TeamCreateDto에 데이터 변환 생성자 추가

--- back-end/src/main/java/com/feedhanjum/back_end/team/service/dto/TeamCreateDto.java
+++ back-end/src/main/java/com/feedhanjum/back_end/team/service/dto/TeamCreateDto.java
@@ -1,6 +1,7 @@
 package com.feedhanjum.back_end.team.service.dto;
 
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
+import com.feedhanjum.back_end.team.controller.dto.TeamCreateRequest;
 
 import java.time.LocalDateTime;
 
@@ -10,4 +11,7 @@ public record TeamCreateDto(
         LocalDateTime endTime,
         FeedbackType feedbackType
 ) {
+    public TeamCreateDto(TeamCreateRequest request) {
+        this(request.name(), request.startTime(), request.endTime(), request.feedbackType());
+    }
 }
