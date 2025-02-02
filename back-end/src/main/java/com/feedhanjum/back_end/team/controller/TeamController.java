db8de20194bbf7873e0f08ebefbbdf92bb440a3e
GoGradually <chkun3109@gmail.com>
2025-02-02 19:55:44 +0900

    [FD-21] feat: 특정 팀의 정보를 반환하는 API 구현
    - 팀원 정보를 포함한 모든 팀 정보를 반환하는 API
    - 팀원 정보만을 반환하는 API

--- back-end/src/main/java/com/feedhanjum/back_end/team/controller/TeamController.java
+++ back-end/src/main/java/com/feedhanjum/back_end/team/controller/TeamController.java
@@ -1,7 +1,10 @@
 package com.feedhanjum.back_end.team.controller;
 
 import com.feedhanjum.back_end.auth.infra.Login;
+import com.feedhanjum.back_end.member.controller.dto.MemberDto;
+import com.feedhanjum.back_end.member.service.MemberService;
 import com.feedhanjum.back_end.team.controller.dto.TeamCreateRequest;
+import com.feedhanjum.back_end.team.controller.dto.TeamDetailResponse;
 import com.feedhanjum.back_end.team.controller.dto.TeamResponse;
 import com.feedhanjum.back_end.team.domain.Team;
 import com.feedhanjum.back_end.team.service.TeamService;
@@ -20,6 +23,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TeamController {
     private final TeamService teamService;
+    private final MemberService memberService;
 
     @PostMapping("/create")
     public ResponseEntity<TeamResponse> createTeam(@Login Long memberId,
@@ -30,12 +34,35 @@ public class TeamController {
         return new ResponseEntity<>(new TeamResponse(team), HttpStatus.CREATED);
     }
 
-    @GetMapping
+    @GetMapping("/my-teams")
     public ResponseEntity<List<TeamResponse>> getMyTeams(@Login Long memberId) {
         List<TeamResponse> teams = teamService.getMyTeams(memberId)
                 .stream()
                 .map(TeamResponse::new)
                 .collect(Collectors.toList());
         return ResponseEntity.ok(teams);
+    }
+
+    /**
+     * 팀 스페이스 관리 화면에서 쓰일 거 같아서 만들긴 했는데, 확실치 않아 주석 달아둡니다.
+     *
+     * @return 팀 정보 + 속한 회원 정보를 반환
+     */
+    @GetMapping("/{teamId}")
+    public ResponseEntity<TeamDetailResponse> getTeamDetail(@Login Long memberId, @PathVariable Long teamId) {
+        Team team = teamService.getTeam(teamId);
+        List<MemberDto> membersByTeam = memberService.getMembersByTeam(memberId, teamId)
+                .stream().map(MemberDto::new).toList();
+        TeamDetailResponse teamDetailResponse = new TeamDetailResponse();
+        teamDetailResponse.setTeamResponse(new TeamResponse(team));
+        teamDetailResponse.setMembers(membersByTeam);
+        return ResponseEntity.ok(teamDetailResponse);
+    }
+
+    @GetMapping("/{teamId}/members")
+    public ResponseEntity<List<MemberDto>> getTeamMembers(@Login Long memberId, @PathVariable Long teamId) {
+        List<MemberDto> membersByTeam = memberService.getMembersByTeam(memberId, teamId)
+                .stream().map(MemberDto::new).toList();
+        return ResponseEntity.ok(membersByTeam);
     }
 }
