93a212e61a95fe600b5dc4f9034ae70f0020d936
GoGradually <chkun3109@gmail.com>
2025-02-02 18:48:23 +0900

    [FD-14] test: 자신이 속한 팀 목록 조회 컨트롤러 테스트 코드 작성

--- back-end/src/test/java/com/feedhanjum/back_end/team/controller/TeamControllerTest.java
+++ back-end/src/test/java/com/feedhanjum/back_end/team/controller/TeamControllerTest.java
@@ -2,8 +2,10 @@ package com.feedhanjum.back_end.team.controller;
 
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
 import com.feedhanjum.back_end.member.controller.dto.MemberDto;
+import com.feedhanjum.back_end.member.domain.Member;
 import com.feedhanjum.back_end.team.controller.dto.TeamCreateRequest;
 import com.feedhanjum.back_end.team.controller.dto.TeamResponse;
+import com.feedhanjum.back_end.team.domain.Team;
 import com.feedhanjum.back_end.team.service.TeamService;
 import com.feedhanjum.back_end.team.service.dto.TeamCreateDto;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -54,4 +57,22 @@ class TeamControllerTest {
         assertThat(response.getBody()).isEqualTo(teamResponse);
     }
 
+    @Test
+    @DisplayName("내 팀 조회 컨트롤러 테스트")
+    void getMyTeams_팀조회_컨트롤러() {
+        //given
+        Long memberId = 1L;
+        Team team = new Team("haha", new Member("haha", "haha@hoho", null), LocalDateTime.now().plusDays(1),
+                LocalDateTime.now().plusDays(10), FeedbackType.ANONYMOUS);
+        TeamResponse teamResponse = new TeamResponse(team);
+        when(teamService.getMyTeams(memberId)).thenReturn(List.of(team));
+
+        //when
+        ResponseEntity<List<TeamResponse>> response = teamController.getMyTeams(memberId);
+
+        //then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).hasSize(1);
+        assertThat(response.getBody().get(0)).isEqualTo(teamResponse);
+    }
 }
