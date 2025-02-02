26e472f272b4870ce00fc9d2ad7097e539f2bc78
GoGradually <chkun3109@gmail.com>
2025-02-02 18:45:02 +0900

    [FD-14] test: 자신이 속한 팀 목록 조회 서비스 테스트 코드 작성

--- back-end/src/test/java/com/feedhanjum/back_end/team/service/TeamServiceTest.java
+++ back-end/src/test/java/com/feedhanjum/back_end/team/service/TeamServiceTest.java
@@ -2,10 +2,12 @@ package com.feedhanjum.back_end.team.service;
 
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
 import com.feedhanjum.back_end.member.domain.Member;
+import com.feedhanjum.back_end.member.domain.ProfileImage;
 import com.feedhanjum.back_end.member.repository.MemberRepository;
 import com.feedhanjum.back_end.team.domain.Team;
 import com.feedhanjum.back_end.team.domain.TeamMember;
 import com.feedhanjum.back_end.team.repository.TeamMemberRepository;
+import com.feedhanjum.back_end.team.repository.TeamQueryRepository;
 import com.feedhanjum.back_end.team.repository.TeamRepository;
 import com.feedhanjum.back_end.team.service.dto.TeamCreateDto;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,6 +36,9 @@ class TeamServiceTest {
 
     @Mock
     private TeamMemberRepository teamMemberRepository;
+
+    @Mock
+    private TeamQueryRepository teamQueryRepository;
 
     @Mock
     private MemberRepository memberRepository;
@@ -82,4 +88,20 @@ class TeamServiceTest {
                 .hasMessageContaining("프로젝트 시작 시간이 종료 시간보다 앞서야 합니다.");
     }
 
+    @Test
+    @DisplayName("내가 가입한 팀 목록 조회 성공")
+    void getMyTeams_팀조회() {
+        //given
+        Long userId = 1L;
+        Member leader = new Member("haha", "haha@hoho", new ProfileImage("blue", "image1"));
+        Team team = new Team("haha", leader, LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(10), FeedbackType.ANONYMOUS);
+        when(teamQueryRepository.findTeamByMemberId(userId)).thenReturn(List.of(team));
+
+        //when
+        List<Team> result = teamService.getMyTeams(userId);
+
+        //then
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getId()).isEqualTo(team.getId());
+    }
 }
