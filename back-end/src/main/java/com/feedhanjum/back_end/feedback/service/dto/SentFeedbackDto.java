e8aaf72e704e9530b22fc2f0f83642d3c9a9c4f9
vvsos1 <vvsos10@naver.com>
2025-02-02 21:19:16 +0900

    [FD-99] feat: 보낸 피드백을 나타내는 dto 추가

---
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/service/dto/SentFeedbackDto.java
@@ -0,0 +1,41 @@
+package com.feedhanjum.back_end.feedback.service.dto;
+
+import com.feedhanjum.back_end.feedback.domain.Feedback;
+import com.feedhanjum.back_end.feedback.domain.FeedbackType;
+import com.feedhanjum.back_end.feedback.domain.ObjectiveFeedback;
+import com.feedhanjum.back_end.member.domain.Member;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record SentFeedbackDto(
+        Long feedbackId,
+        boolean isAnonymous,
+        Receiver receiver,
+        List<String> objectiveFeedbacks,
+        String subjectiveFeedback,
+        String teamName,
+        LocalDateTime createdAt
+) {
+
+
+    public static SentFeedbackDto from(Feedback feedback) {
+        return new SentFeedbackDto(
+                feedback.getId(),
+                feedback.getFeedbackType() == FeedbackType.ANONYMOUS,
+                Receiver.from(feedback.getReceiver()),
+                feedback.getObjectiveFeedbacks().stream().map(ObjectiveFeedback::getDescription).toList(),
+                feedback.getSubjectiveFeedback(),
+                feedback.getTeam().getName(),
+                feedback.getCreatedAt()
+        );
+    }
+
+
+    public record Receiver(String name, String backgroundColor, String image) {
+        public static Receiver from(Member member) {
+            return new Receiver(member.getName(), member.getProfileImage().getBackgroundColor(), member.getProfileImage().getImage());
+        }
+    }
+
+}
