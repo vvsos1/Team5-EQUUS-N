fb555c9cf3446380186ad6f9792e789068f5be0c
hammsik <asbbrp@gmail.com>
2025-02-04 00:11:27 +0900

    [FD-196] feat: 팀스페이스 선택 드롭다운 컴포넌트 구현

--- front-end/src/components/Dropdown.jsx
+++ front-end/src/components/Dropdown.jsx
@@ -1,10 +1,44 @@
-export default function Dropdown({ triggerText, items }) {
-  return (
-    <details>
-      <summary className='rounded-200 cursor-pointer list-none bg-gray-700 px-5 py-7'>
-        {triggerText}
-      </summary>
-      <p>Here's some content</p>
-    </details>
-  );
+import { useRef, useState } from 'react';
+import Icon from './Icon';
+
+export default function Dropdown({ triggerText, items, isSmall = false }) {
+  const detailsRef = useRef(null);
+  const [triggerTextState, setTriggerTextState] = useState(triggerText);
+
+  if (isSmall) {
+    return (
+      <details ref={detailsRef} className='group'>
+        <summary className='rounded-200 caption-2 flex size-fit cursor-pointer list-none items-center bg-gray-700 py-1 pr-1 pl-2.5 text-gray-200'>
+          {triggerTextState}
+          <Icon
+            name='chevronDown'
+            className='ml-0.5 transition group-open:rotate-180'
+          />
+        </summary>
+        <ul className='rounded-200 absolute z-10 mt-2 flex size-fit flex-col gap-1 bg-gray-700 p-2'>
+          {[...items, '전체 보기'].map((item, index) => (
+            <li
+              key={index}
+              onClick={() => {
+                detailsRef.current.open = false;
+                setTriggerTextState(item);
+              }}
+              className={`body-1 cursor-pointer list-none px-2.5 py-1 ${item === triggerTextState ? 'text-gray-200' : 'text-gray-400'}`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </details>
+    );
+  } else {
+    return (
+      <details ref={detailsRef}>
+        <summary className='rounded-200 caption-2 size-fit cursor-pointer list-none bg-gray-700 px-2.5 py-1 text-gray-200'>
+          {triggerText}
+        </summary>
+        <p>Here's some content</p>
+      </details>
+    );
+  }
 }
--- front-end/src/pages/feedback/FeedbackRequest.jsx
+++ front-end/src/pages/feedback/FeedbackRequest.jsx
@@ -12,13 +12,17 @@ export default function FeedbackRequest() {
           console.log('pop click');
         }}
       />
+      <Dropdown
+        triggerText={'전체 보기'}
+        isSmall={true}
+        items={['프론트엔드', '백엔드', '데이터', '디자인', '기획']}
+      />
       <h1 className='header-2 text-gray-0 my-6 whitespace-pre-line'>
         {'백현식님에게 요청할\n피드백을 작성해주세요'}
       </h1>
       <TextArea isWithGpt={true} gene={true} />
       <TextArea isWithAi={true} />
       <TextArea isWithAi={true} canToggleAnonymous={true} />
-      <Dropdown />
     </div>
   );
 }
