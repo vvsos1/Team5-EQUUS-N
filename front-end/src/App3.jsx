a867894412be6e0f8326c34a8448cb920994c427
lsj1137 <lsj1137@naver.com>
2025-02-03 16:28:39 +0900

    [FD-178] chore: 브랜치 분리를 위한 커밋 내역 삭제

--- front-end/src/App3.jsx
+++
@@ -1,8 +0,0 @@
-function App3() {
-  return (
-    <div className='mx-5 flex h-screen w-sm items-center justify-center bg-gray-900'>
-    </div>
-  );
-}
-
-export default App3;
---
+++ front-end/src/pages/auth/SignUp.jsx
@@ -0,0 +1,109 @@
+import NavBar from './components/NavBar';
+import CustomInput from '../../components/CustomInput';
+import LargeButton from '../../components/buttons/LargeButton';
+import Icon from '../../components/Icon';
+import Certification from '../../components/Certification';
+import { CertState } from '../../components/Certification';
+import { useEffect, useState } from 'react';
+
+export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [certState, setCertState] = useState(CertState.BEFORE_SEND_CODE);
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isPasswordConfirmVisible, setIsPasswordConfirmVisible] =
+    useState(false);
+  const [nickName, setNickName] = useState('');
+
+  useEffect(() => {
+    setNickName(nickName.trim());
+  }, [nickName]);
+
+  return (
+    <div className='relative flex h-screen w-full flex-col justify-start'>
+      <NavBar title='계정 만들기' />
+      <div className='h-2' />
+      <CustomInput
+        label='이메일'
+        hint='이메일을 입력해 주세요'
+        content={email}
+        setContent={certState !== CertState.AFTER_CHECK_CODE && setEmail}
+        type='email'
+      />
+      <div className='h-4' />
+      <Certification
+        email={/^\S+@\S+$/.test(email) ? email : ''}
+        certState={certState}
+        setCertState={setCertState}
+      />
+      <div className='h-6' />
+      <CustomInput
+        label='비밀번호'
+        hint='영문, 숫자 포함 8글자 이상'
+        content={password}
+        setContent={setPassword}
+        isPassword={!isPasswordVisible}
+        condition={[
+          (content) =>
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/.test(content),
+          (content) => content.length >= 8,
+        ]}
+        notification={['영문, 숫자, 특수 문자 포함', '8글자 이상']}
+        addOn={
+          <button onClick={() => setIsPasswordVisible(!isPasswordVisible)}>
+            <Icon
+              name='eye'
+              color={
+                !isPasswordVisible ?
+                  'var(--color-gray-500)'
+                : 'var(--color-lime-500)'
+              }
+            />
+          </button>
+        }
+      />
+      <CustomInput
+        label='비밀번호 확인'
+        hint='비밀번호를 재입력해주세요'
+        content={passwordConfirm}
+        setContent={setPasswordConfirm}
+        isPassword={!isPasswordConfirmVisible}
+        addOn={
+          <button
+            onClick={() =>
+              setIsPasswordConfirmVisible(!isPasswordConfirmVisible)
+            }
+          >
+            <Icon
+              name={
+                passwordConfirm.length > 0 && passwordConfirm === password ?
+                  'checkBoxClick'
+                : 'checkBoxNone'
+              }
+            />
+          </button>
+        }
+      />
+      <div className='h-6' />
+      <CustomInput
+        label='활동 이름'
+        hint='팀원이 보게 될 이름이에요'
+        content={nickName}
+        setContent={setNickName}
+        addOn={
+          <Icon
+            name={
+              nickName.length > 0 && nickName.length < 10 ?
+                'checkBoxClick'
+              : 'checkBoxNone'
+            }
+          />
+        }
+      />
+      <div className='absolute right-0 bottom-[34px] left-0 h-20 bg-gray-900'>
+        <LargeButton text='다음' isOutlined={false} />
+      </div>
+    </div>
+  );
+}
