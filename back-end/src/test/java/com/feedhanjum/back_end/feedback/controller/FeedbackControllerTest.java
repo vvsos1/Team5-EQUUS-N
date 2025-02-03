db6c34ed168f173b1a624caa72f4bddaa3be5b73
vvsos1 <vvsos10@naver.com>
2025-02-03 18:10:45 +0900

    [FD-246] test: 수시 피드백 요청 조회 controller 테스트 추가

--- back-end/src/test/java/com/feedhanjum/back_end/feedback/controller/FeedbackControllerTest.java
+++ back-end/src/test/java/com/feedhanjum/back_end/feedback/controller/FeedbackControllerTest.java
@@ -1,15 +1,19 @@
 package com.feedhanjum.back_end.feedback.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.feedhanjum.back_end.auth.infra.SessionConst;
 import com.feedhanjum.back_end.feedback.controller.dto.request.FrequentFeedbackRequestForApiRequest;
+import com.feedhanjum.back_end.feedback.controller.dto.request.FrequentFeedbackRequestQueryRequest;
 import com.feedhanjum.back_end.feedback.controller.dto.request.FrequentFeedbackSendRequest;
 import com.feedhanjum.back_end.feedback.controller.dto.request.RegularFeedbackSendRequest;
+import com.feedhanjum.back_end.feedback.controller.dto.response.FrequentFeedbackRequestDto;
 import com.feedhanjum.back_end.feedback.domain.Feedback;
 import com.feedhanjum.back_end.feedback.domain.FeedbackFeeling;
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
 import com.feedhanjum.back_end.feedback.repository.FeedbackRepository;
 import com.feedhanjum.back_end.member.domain.Member;
+import com.feedhanjum.back_end.member.domain.ProfileImage;
 import com.feedhanjum.back_end.member.repository.MemberRepository;
 import com.feedhanjum.back_end.schedule.domain.RegularFeedbackRequest;
 import com.feedhanjum.back_end.schedule.domain.Schedule;
@@ -18,6 +22,7 @@ import com.feedhanjum.back_end.schedule.domain.ScheduleRole;
 import com.feedhanjum.back_end.schedule.repository.RegularFeedbackRequestRepository;
 import com.feedhanjum.back_end.schedule.repository.ScheduleMemberRepository;
 import com.feedhanjum.back_end.schedule.repository.ScheduleRepository;
+import com.feedhanjum.back_end.team.domain.FrequentFeedbackRequest;
 import com.feedhanjum.back_end.team.domain.Team;
 import com.feedhanjum.back_end.team.domain.TeamMember;
 import com.feedhanjum.back_end.team.repository.FrequentFeedbackRequestRepository;
@@ -76,18 +81,22 @@ class FeedbackControllerTest {
 
     private Member member1;
     private Member member2;
+    private Member member3;
     private Team team1;
     private TeamMember teamMember1;
     private TeamMember teamMember2;
+    private TeamMember teamMember3;
     private Schedule schedule1;
     private ScheduleMember scheduleMember1;
     private ScheduleMember scheduleMember2;
+    private ScheduleMember scheduleMember3;
 
     @BeforeEach
     void setUp() {
-        member1 = new Member("member1", "email1@email.com", null);
-        member2 = new Member("member2", "email2@email.com", null);
-        memberRepository.saveAll(List.of(member1, member2));
+        member1 = new Member("member1", "email1@email.com", new ProfileImage("bg1", "character1"));
+        member2 = new Member("member2", "email2@email.com", new ProfileImage("bg2", "character2"));
+        member3 = new Member("member3", "email3@email.com", new ProfileImage("bg3", "character3"));
+        memberRepository.saveAll(List.of(member1, member2, member3));
 
         team1 = new Team("team1", member1, LocalDateTime.now(), LocalDateTime.now().plusDays(1), FeedbackType.ANONYMOUS);
 
@@ -95,14 +104,16 @@ class FeedbackControllerTest {
 
         teamMember1 = new TeamMember(team1, member1);
         teamMember2 = new TeamMember(team1, member2);
-        teamMemberRepository.saveAll(List.of(teamMember1, teamMember2));
+        teamMember3 = new TeamMember(team1, member3);
+        teamMemberRepository.saveAll(List.of(teamMember1, teamMember2, teamMember3));
 
         schedule1 = new Schedule("schedule1", LocalDateTime.now(), LocalDateTime.now().plusDays(1), team1);
         scheduleRepository.save(schedule1);
 
         scheduleMember1 = new ScheduleMember(ScheduleRole.OWNER, schedule1, member1);
         scheduleMember2 = new ScheduleMember(ScheduleRole.MEMBER, schedule1, member2);
-        scheduleMemberRepository.saveAll(List.of(scheduleMember1, scheduleMember2));
+        scheduleMember3 = new ScheduleMember(ScheduleRole.MEMBER, schedule1, member3);
+        scheduleMemberRepository.saveAll(List.of(scheduleMember1, scheduleMember2, scheduleMember3));
 
     }
 
@@ -301,7 +312,7 @@ class FeedbackControllerTest {
             Member sender = member1;
             Member receiver = member2;
             Team team = team1;
-            String requestedContent = "테스트 요청 내용";
+            String requestedContent = "내용";
 
             var body = new FrequentFeedbackRequestForApiRequest(receiver.getId(), team.getId(), requestedContent);
 
@@ -324,5 +335,42 @@ class FeedbackControllerTest {
             assertThat(request.getCreatedAt()).isCloseTo(LocalDateTime.now(), within(1, ChronoUnit.SECONDS));
         }
 
+    }
+
+    @Nested
+    @DisplayName("수시 피드백 요청 조회 테스트")
+    class GetFrequentFeedbackRequest {
+
+        @Test
+        @DisplayName("성공 시 200")
+        void test1() throws Exception {
+            // given
+            Member sender1 = member1;
+            Member sender2 = member3;
+            TeamMember teamMember = teamMember2;
+            Member receiver = teamMember.getMember();
+            var request = new FrequentFeedbackRequestQueryRequest(teamMember.getTeam().getId());
+            frequentFeedbackRequestRepository.save(new FrequentFeedbackRequest("내용1", teamMember, sender1));
+            frequentFeedbackRequestRepository.save(new FrequentFeedbackRequest("내용2", teamMember, sender2));
+
+            // when
+            assertThat(mvc.get()
+                    .uri("/api/feedbacks/regular/request")
+                    .session(withLoginUser(receiver))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(mapper.writeValueAsString(request))
+            ).hasStatus(HttpStatus.OK)
+                    .body()
+                    .satisfies(result -> {
+                        List<FrequentFeedbackRequestDto> requests = mapper.readValue(result, new TypeReference<List<FrequentFeedbackRequestDto>>() {
+                        });
+                        assertThat(requests).hasSize(2);
+                        assertThat(requests).extracting(FrequentFeedbackRequestDto::requestedContent)
+                                .containsExactlyInAnyOrder("내용1", "내용2");
+                        assertThat(requests).extracting(req -> req.requester().email())
+                                .containsExactlyInAnyOrder(sender1.getEmail(), sender2.getEmail());
+                    });
+
+        }
     }
 }
