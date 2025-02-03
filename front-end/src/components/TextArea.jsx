c3ea435a75ea680f91cdcc4e8d11c0940b5fdad0
hammsik <asbbrp@gmail.com>
2025-02-03 18:18:46 +0900

    [FD-196] feat: textarea 컴포넌트 제작

--- front-end/src/components/TextArea.jsx
+++ front-end/src/components/TextArea.jsx
@@ -1,24 +1,45 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
+import Icon from './Icon';
 
-export default function TextArea() {
-  const [textLength, dispatch] = useReducer(
-    (state, eventTarget) => eventTarget.value.length,
-    0,
-  );
+export default function TextArea({
+  isWithGpt = false,
+  canToggleAnonymous = false,
+  generatedByGpt = false,
+}) {
+  const [textLength, setTextLength] = useState(0);
+  const [isAnonymous, toggleAnonymous] = useReducer((state) => !state, false);
 
   const onInput = (e) => {
     e.target.style.height = 'auto';
     e.target.style.height = `${e.target.scrollHeight}px`;
-    dispatch(e.target);
+    setTextLength(e.target.value.length);
   };
+
   return (
     <div className='relative'>
       <textarea
         onInput={onInput}
-        className='text-gray-0 placeholder:body-1 rounded-300 relative w-full resize-none p-5 pb-10 ring ring-gray-500 outline-none focus:ring-gray-300'
-        placeholder='여기에 적어주세요'
+        className={`text-gray-0 placeholder:body-1 rounded-300 relative min-h-44 w-full resize-none p-5 pb-14 ring-gray-500 outline-none focus:ring-gray-300 ${generatedByGpt ? 'bg-gray-800' : 'ring'}`}
+        placeholder={
+          !generatedByGpt &&
+          (isWithGpt ?
+            '자유롭게 적고 AI를 통해 다듬어 보세요.(선택사항)'
+          : '여기에 적어주세요')
+        }
+        disabled={isWithGpt}
       />
       <p className='caption-1 absolute right-5 bottom-5 text-gray-300'>{`${textLength}/400 byte`}</p>
+      {canToggleAnonymous && (
+        <button
+          className='absolute bottom-5 left-5 flex items-center'
+          onClick={toggleAnonymous}
+        >
+          <p className='body-1 mr-1.5 text-white'>익명</p>
+          {isAnonymous ?
+            <Icon name='checkBoxClick' />
+          : <Icon name='checkBoxNone' />}
+        </button>
+      )}
     </div>
   );
 }
--- front-end/src/pages/feedback/FeedbackRequest.jsx
+++ front-end/src/pages/feedback/FeedbackRequest.jsx
@@ -2,7 +2,7 @@ import { useReducer } from 'react';
 import NavBar2 from '../../components/NavBar2';
 import TextArea from '../../components/TextArea';
 
-export default function FeedbackRequest({}) {
+export default function FeedbackRequest() {
   return (
     <div className='flex size-full flex-col'>
       <NavBar2
@@ -14,7 +14,9 @@ export default function FeedbackRequest({}) {
       <h1 className='header-2 text-gray-0 my-6 whitespace-pre-line'>
         {'백현식님에게 요청할\n피드백을 작성해주세요'}
       </h1>
-      <TextArea />
+      <TextArea isWithGpt={true} generatedByGpt={true} />
+      <TextArea isWithAi={true} />
+      <TextArea isWithAi={true} canToggleAnonymous={true} />
     </div>
   );
 }
