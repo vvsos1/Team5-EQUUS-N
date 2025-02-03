ba8b5c5b0fe3af934711394670cc918c81362c8b
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-03 18:38:39 +0900

    [FD-26] feat: 일정 생성을 처리하는 컨트롤러 코드 구현

---
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/controller/ScheduleController.java
@@ -0,0 +1,25 @@
+package com.feedhanjum.back_end.schedule.controller;
+
+import com.feedhanjum.back_end.auth.infra.Login;
+import com.feedhanjum.back_end.schedule.controller.dto.ScheduleCreateRequest;
+import com.feedhanjum.back_end.schedule.service.ScheduleService;
+import com.feedhanjum.back_end.schedule.service.dto.ScheduleRequestDto;
+import com.feedhanjum.back_end.schedule.service.dto.ScheduleResponseDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/team/{teamId}/schedule")
+public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ScheduleResponseDto> createSchedule(@Login Long memberId, @PathVariable Long teamId, @Valid @RequestBody ScheduleCreateRequest request) {
+        ScheduleResponseDto schedule = scheduleService.createSchedule(memberId, teamId, new ScheduleRequestDto(request));
+        return new ResponseEntity<>(schedule, HttpStatus.CREATED);
+    }
+}
