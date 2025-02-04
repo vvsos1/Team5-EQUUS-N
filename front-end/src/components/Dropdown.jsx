3d5a2455d982fe52f24926de09ef293fc0e0b11c
hammsik <asbbrp@gmail.com>
2025-02-04 11:29:02 +0900

    [FD-196] feat: Dropdown 컴포넌트 분리 및 컴포넌트를 인자로 받을 수 있도록

--- front-end/src/components/Dropdown.jsx
+++ front-end/src/components/Dropdown.jsx
@@ -1,78 +1,91 @@
 import { useRef, useState } from 'react';
 import Icon from './Icon';
 
-export default function Dropdown({
-  triggerText = '',
-  items,
-  isSmall = false,
+export function DropdownSmall({ triggerText, setTriggerText, items }) {
+  const detailsRef = useRef(null);
+
+  return (
+    <details ref={detailsRef} className='group'>
+      <summary className='rounded-200 caption-2 flex size-fit cursor-pointer list-none items-center bg-gray-700 py-1 pr-1 pl-2.5 text-gray-200'>
+        {triggerText}
+        <Icon
+          name='chevronDown'
+          className='ml-0.5 transition group-open:rotate-180'
+        />
+      </summary>
+      <ul className='rounded-200 absolute z-20 mt-2 flex size-fit flex-col gap-1 bg-gray-700 p-2'>
+        {[...items, '전체 보기'].map((item, index) => (
+          <li
+            key={index}
+            onClick={() => {
+              detailsRef.current.open = false;
+              setTriggerText(item);
+            }}
+            className={`body-1 cursor-pointer list-none px-2.5 py-1 ${item === triggerText ? 'text-gray-200' : 'text-gray-400'}`}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      {/* 배경 클릭을 위한 빽드롭필터 */}
+      <div
+        className='fixed inset-0 z-10'
+        onClick={() => (detailsRef.current.open = false)}
+      />
+    </details>
+  );
+}
+
+export function DropdownLarge({
+  triggerText,
+  setTriggerText,
   isFromTime = true,
+  isTransparent = false,
+  itemsComponent,
+  items,
 }) {
   const detailsRef = useRef(null);
-  const [triggerTextState, setTriggerTextState] = useState(triggerText);
 
-  if (isSmall) {
-    return (
-      <details ref={detailsRef} className='group'>
-        <summary className='rounded-200 caption-2 flex size-fit cursor-pointer list-none items-center bg-gray-700 py-1 pr-1 pl-2.5 text-gray-200'>
-          {triggerTextState}
-          <Icon
-            name='chevronDown'
-            className='ml-0.5 transition group-open:rotate-180'
-          />
-        </summary>
-        <ul className='rounded-200 absolute z-10 mt-2 flex size-fit flex-col gap-1 bg-gray-700 p-2'>
-          {[...items, '전체 보기'].map((item, index) => (
-            <li
-              key={index}
-              onClick={() => {
-                detailsRef.current.open = false;
-                setTriggerTextState(item);
-              }}
-              className={`body-1 cursor-pointer list-none px-2.5 py-1 ${item === triggerTextState ? 'text-gray-200' : 'text-gray-400'}`}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-        {/* 배경 클릭을 위한 빽드롭필터 */}
-        <div
-          className='fixed inset-0'
-          onClick={() => (detailsRef.current.open = false)}
+  return (
+    <details ref={detailsRef} className='group flex-1'>
+      <summary
+        className={`${isTransparent ? 'border border-gray-300' : 'bg-gray-700'} rounded-200 body-1 flex size-fit w-full cursor-pointer list-none items-center justify-between px-5 py-3.5 whitespace-pre text-gray-200`}
+      >
+        <p>
+          {triggerText} <span>{isFromTime ? ' 부터' : ' 까지'}</span>
+        </p>
+        <Icon
+          name='chevronDown'
+          className='ml-0.5 transition group-open:rotate-180'
         />
-      </details>
-    );
-  } else {
-    return (
-      <details ref={detailsRef} className='group'>
-        <summary className='rounded-200 caption-2 flex size-fit cursor-pointer list-none items-center bg-gray-700 py-1 pr-1 pl-2.5 whitespace-pre text-gray-200'>
-          {triggerTextState} <span>{isFromTime ? ' 부터' : ' 까지'}</span>
-          <Icon
-            name='chevronDown'
-            className='ml-0.5 transition group-open:rotate-180'
-          />
-        </summary>
-        <ul className='rounded-200 absolute z-10 mt-2 flex size-fit max-h-56 flex-col gap-1 overflow-y-auto bg-gray-700 p-2'>
-          {[
-            //TODO: 시간대 배열
-          ].map((item, index) => (
-            <li
-              key={index}
-              onClick={() => {
-                detailsRef.current.open = false;
-                setTriggerTextState(item);
-              }}
-              className={`body-1 cursor-pointer list-none px-2.5 py-1 ${item === triggerTextState ? 'text-gray-200' : 'text-gray-400'}`}
-            >
-              {item}
-            </li>
-          ))}
-        </ul>
-        {/* 배경 클릭을 위한 빽드롭필터 */}
-        <div
-          className='fixed inset-0'
-          onClick={() => (detailsRef.current.open = false)}
-        />
-      </details>
-    );
-  }
+      </summary>
+      <div className='absolute z-20 mt-2'>
+        {itemsComponent ?
+          itemsComponent
+        : <ul className='rounded-200 flex size-fit max-h-56 flex-col gap-1 overflow-y-auto bg-gray-700 p-2'>
+            {
+              //TODO: 시간대 배열
+              items.map((item, index) => (
+                <li
+                  key={index}
+                  onClick={() => {
+                    detailsRef.current.open = false;
+                    setTriggerText(item);
+                  }}
+                  className={`body-1 cursor-pointer list-none px-2.5 py-1 ${item === triggerText ? 'text-gray-200' : 'text-gray-400'}`}
+                >
+                  {item}
+                </li>
+              ))
+            }
+          </ul>
+        }
+      </div>
+      {/* 배경 클릭을 위한 빽드롭필터 */}
+      <div
+        className='fixed inset-0 z-10'
+        onClick={() => (detailsRef.current.open = false)}
+      />
+    </details>
+  );
 }
--- front-end/src/pages/feedback/FeedbackRequest.jsx
+++ front-end/src/pages/feedback/FeedbackRequest.jsx
@@ -1,9 +1,11 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import NavBar2 from '../../components/NavBar2';
 import TextArea from '../../components/TextArea';
-import Dropdown from '../../components/Dropdown';
+import { DropdownLarge, DropdownSmall } from '../../components/Dropdown';
+import Notification, { notiType } from '../main/components/notification';
 
 export default function FeedbackRequest() {
+  const [triggerText, setTriggerText] = useState('1/24');
   return (
     <div className='flex size-full flex-col'>
       <NavBar2
@@ -12,20 +14,33 @@ export default function FeedbackRequest() {
           console.log('pop click');
         }}
       />
-      <Dropdown
+      <DropdownSmall
         triggerText={'전체 보기'}
-        isSmall={true}
+        setTriggerText={() => {}}
         items={['프론트엔드', '백엔드', '데이터', '디자인', '기획']}
       />
-      <Dropdown
-        isSmall={false}
-        items={['프론트엔드', '백엔드', '데이터', '디자인', '기획']}
-      />
-      <Dropdown
-        isSmall={false}
+      <DropdownLarge
+        triggerText={'프론트엔드'}
+        setTriggerText={() => {}}
         isFromTime={false}
         items={['프론트엔드', '백엔드', '데이터', '디자인', '기획']}
       />
+      <div className='flex w-full gap-4'>
+        <DropdownLarge
+          triggerText={triggerText}
+          setTriggerText={setTriggerText}
+          isFromTime={true}
+          isTransparent={true}
+          itemsComponent={<Notification type={notiType.UNCONFIRM} />}
+        />
+        <DropdownLarge
+          triggerText={triggerText}
+          setTriggerText={setTriggerText}
+          isFromTime={false}
+          isTransparent={true}
+          items={['1/25', '1/26', '1/27', '1/28', '1/29']}
+        />
+      </div>
       <h1 className='header-2 text-gray-0 my-6 whitespace-pre-line'>
         {'백현식님에게 요청할\n피드백을 작성해주세요'}
       </h1>
