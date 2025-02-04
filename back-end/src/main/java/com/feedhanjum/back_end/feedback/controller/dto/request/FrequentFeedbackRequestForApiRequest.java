27fa8f16165b33354f4af0bba1be69aa4aaa04ac
vvsos1 <vvsos10@naver.com>
2025-02-04 10:04:24 +0900

    [FD-246] docs: feedback 관련 dto에 swagger 설명 추가

--- back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/dto/request/FrequentFeedbackRequestForApiRequest.java
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/dto/request/FrequentFeedbackRequestForApiRequest.java
@@ -2,15 +2,19 @@ package com.feedhanjum.back_end.feedback.controller.dto.request;
 
 import com.feedhanjum.back_end.core.constraints.ByteLength;
 import com.feedhanjum.back_end.team.domain.FrequentFeedbackRequest;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record FrequentFeedbackRequestForApiRequest(
+        @Schema(description = "수시 피드백 요청의 수신자 ID")
         @NotNull
         Long receiverId,
 
+        @Schema(description = "수시 피드백 요청을 보낼 소속 팀 ID")
         @NotNull
         Long teamId,
 
+        @Schema(description = "피드백 요청 내용", example = "팀원들의 업무에 대한 피드백을 요청합니다.")
         @ByteLength(min = FrequentFeedbackRequest.MIN_REQUESTED_CONTENT_BYTE, max = FrequentFeedbackRequest.MAX_REQUESTED_CONTENT_BYTE, strip = true)
         String requestedContent
 ) {
--- back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/dto/request/FrequentFeedbackSendRequest.java
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/dto/request/FrequentFeedbackSendRequest.java
@@ -4,6 +4,7 @@ import com.feedhanjum.back_end.core.constraints.ByteLength;
 import com.feedhanjum.back_end.feedback.domain.Feedback;
 import com.feedhanjum.back_end.feedback.domain.FeedbackFeeling;
 import com.feedhanjum.back_end.feedback.domain.ObjectiveFeedback;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,22 +12,28 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record FrequentFeedbackSendRequest(
+        @Schema(description = "수시 피드백을 받을 사람의 ID")
         @NotNull
         Long receiverId,
 
+        @Schema(description = "연관된 팀 ID")
         @NotNull
         Long teamId,
 
+        @Schema(description = "피드백의 방향성")
         @NotNull
         FeedbackFeeling feedbackFeeling,
 
+        @Schema(description = "객관식 피드백 목록")
         @Size(min = Feedback.MIN_OBJECTIVE_FEEDBACK_SIZE, max = Feedback.MAX_OBJECTIVE_FEEDBACK_SIZE)
         List<ObjectiveFeedback> objectiveFeedbacks,
 
+        @Schema(description = "주관식 피드백")
         @NotBlank
         @ByteLength(min = Feedback.MIN_SUBJECTIVE_FEEDBACK_BYTE, max = Feedback.MAX_SUBJECTIVE_FEEDBACK_BYTE, strip = true)
         String subjectiveFeedback,
 
+        @Schema(description = "익명 여부")
         @NotNull
         Boolean isAnonymous
 ) {
