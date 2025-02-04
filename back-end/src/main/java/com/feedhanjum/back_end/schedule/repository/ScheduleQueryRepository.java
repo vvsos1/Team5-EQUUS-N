773b4b8a82cdca5e4ab1e3f7babb20bf937e0d00
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-04 11:41:35 +0900

    [FD-29] feat: 일정 관련 쿼리를 위한, ScheduleQueryRepository 생성

---
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleQueryRepository.java
@@ -0,0 +1,44 @@
+package com.feedhanjum.back_end.schedule.repository;
+
+import com.feedhanjum.back_end.schedule.domain.Schedule;
+import com.feedhanjum.back_end.schedule.domain.ScheduleMember;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.feedhanjum.back_end.member.domain.QMember.member;
+import static com.feedhanjum.back_end.schedule.domain.QSchedule.schedule;
+import static com.feedhanjum.back_end.schedule.domain.QScheduleMember.scheduleMember;
+import static com.feedhanjum.back_end.schedule.domain.QTodo.todo;
+
+@Repository
+@RequiredArgsConstructor
+public class ScheduleQueryRepository {
+    private final JPAQueryFactory queryFactory;
+
+    public List<ScheduleMember> findScheduleTodoList(Long scheduleId, Long memberId) {
+        return queryFactory.select(scheduleMember)
+                .from(schedule)
+                .join(schedule.scheduleMembers, scheduleMember)
+                .join(scheduleMember.todos, todo)
+                .join(scheduleMember.member, member)
+                .fetchJoin()
+                .where(schedule.id.eq(scheduleId), memberIdEq(memberId))
+                .fetch();
+    }
+
+    public Schedule findScheduleByNearestTime(Long teamId, LocalDateTime time){
+        return queryFactory.selectFrom(schedule)
+                .where(schedule.team.id.eq(teamId), schedule.startTime.after(time))
+                .orderBy(schedule.startTime.asc())
+                .fetchFirst();
+    }
+
+    private BooleanExpression memberIdEq(Long memberId){
+        return memberId == null ? null : member.id.eq(memberId);
+    }
+}
