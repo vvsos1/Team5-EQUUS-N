b2752db72dc2d3fbc580dcb965dcd63ac934b67d
vvsos1 <vvsos10@naver.com>
2025-02-05 14:34:05 +0900

    [FD-274] fix: swagger 수정

--- back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/FeedbackRefinementController.java
+++ back-end/src/main/java/com/feedhanjum/back_end/feedback/controller/FeedbackRefinementController.java
@@ -4,6 +4,7 @@ import com.feedhanjum.back_end.feedback.controller.dto.request.FeedbackRefineReq
 import com.feedhanjum.back_end.feedback.controller.dto.response.FeedbackRefineResponse;
 import com.feedhanjum.back_end.feedback.controller.dto.response.RefineRemainCountResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
@@ -18,7 +19,7 @@ public class FeedbackRefinementController {
     @Operation(summary = "주관식 피드백 다듬기", description = "AI를 활용해 주관식 피드백을 다듬습니다. 호출 가능 횟수 제한은 3회입니다. 피드백 작성 시 호출 가능 횟수 제한이 초기화됩니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "주관식 피드백 다듬기 성공"),
-            @ApiResponse(responseCode = "429", description = "호출 가능 횟수 초과")
+            @ApiResponse(responseCode = "429", description = "호출 가능 횟수 초과", content = @Content)
     })
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<FeedbackRefineResponse> refineFeedback(@Valid @RequestBody FeedbackRefineRequest request) {
