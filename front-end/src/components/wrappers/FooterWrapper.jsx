efa0a21f5df50a220479d17d0a4e113163f2dfd7
hammsik <asbbrp@gmail.com>
2025-02-05 17:50:18 +0900

    [FD-281] feat: FooterWrapper 컴포넌트 개발 및 wrapper 폴더 생성

---
+++ front-end/src/components/wrappers/FooterWrapper.jsx
@@ -0,0 +1,10 @@
+export default function FooterWrapper({ bgColor = 'gray-900', children }) {
+  return (
+    <footer className='fixed right-0 bottom-0 left-0 mx-5 pt-3 pb-8'>
+      {children}
+      <div
+        className={`absolute bottom-0 -z-10 h-full bg-${bgColor} left-[50%] w-screen -translate-x-[50%]`}
+      />
+    </footer>
+  );
+}
