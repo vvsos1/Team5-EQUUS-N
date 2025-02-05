3b059bc9256dc74507a511da4f6cc5f435ae1973
hammsik <asbbrp@gmail.com>
2025-02-05 17:53:42 +0900

    [FD-281] feat: FooterWrapper 컴포넌트 개발에 JSDoc 추가

--- front-end/src/components/wrappers/FooterWrapper.jsx
+++ front-end/src/components/wrappers/FooterWrapper.jsx
@@ -1,3 +1,10 @@
+/**
+ * FooterWrapper component
+ * @param {object} props
+ * @param {string} props.bgColor
+ * @param {ReactElement} props.children
+ * @returns {ReactElement}
+ */
 export default function FooterWrapper({ bgColor = 'gray-900', children }) {
   return (
     <footer className='fixed right-0 bottom-0 left-0 mx-5 pt-3 pb-8'>
