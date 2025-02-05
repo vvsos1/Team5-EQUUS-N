72a7b936154e490fcb36087beaf3adbb8f94bbd7
hammsik <asbbrp@gmail.com>
2025-02-05 17:34:09 +0900

    [FD-281] feat: StickyWrapper 컴포넌트 개발

---
+++ front-end/src/components/StickyWrapper.jsx
@@ -0,0 +1,17 @@
+/**
+ * sticky wrapper component
+ * @param {object} props
+ * @param {css} props.bgColor
+ * @param {ReactElement} props.children
+ * @returns {ReactElement}
+ */
+export default function StickyWrapper({ bgColor = 'gray-900', children }) {
+  return (
+    <div className={`sticky top-0 z-[999] w-full`}>
+      {children}
+      <div
+        className={`absolute top-0 -z-10 h-full bg-${bgColor} left-[50%] w-screen -translate-x-[50%]`}
+      />
+    </div>
+  );
+}
