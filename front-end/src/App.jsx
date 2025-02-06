f09d01b20cfdcedc9bd64d70933ed5ed2ee55721
hammsik <asbbrp@gmail.com>
2025-02-06 14:15:14 +0900

    [FD-286] chore: App.js에 QueryClient 적용

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import 'react-datepicker/dist/react-datepicker.css';
 import './index.css';
@@ -12,32 +13,36 @@ import TeamSpaceMake from './pages/teamspace/TeamSpaceMake';
 import TeamSpaceMakeSuccess from './pages/teamspace/TeamSpaceMakeSuccess';
 import MainPage from './pages/main/MainPage';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<Splash />} />
-          <Route path='feedback'>
-            <Route path='request' element={<FeedbackRequest />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Splash />} />
+            <Route path='feedback'>
+              <Route path='request' element={<FeedbackRequest />} />
+            </Route>
+            <Route path='signin' element={<SignIn />} />
+            <Route path='signup' element={<SignUp />} />
+            <Route path='teamspace/make' element={<TeamSpaceMake />} />
+            <Route
+              path='teamspace/make/first'
+              element={<TeamSpaceMake isFirst={true} />}
+            />
+            <Route
+              path='teamspace/make/success'
+              element={<TeamSpaceMakeSuccess />}
+            />
+            <Route path='main'>
+              <Route index element={<MainPage />} />
+              <Route path='notification' element={<div></div>} />
+            </Route>
           </Route>
-          <Route path='signin' element={<SignIn />} />
-          <Route path='signup' element={<SignUp />} />
-          <Route path='teamspace/make' element={<TeamSpaceMake />} />
-          <Route
-            path='teamspace/make/first'
-            element={<TeamSpaceMake isFirst={true} />}
-          />
-          <Route
-            path='teamspace/make/success'
-            element={<TeamSpaceMakeSuccess />}
-          />
-          <Route path='main'>
-            <Route index element={<MainPage />} />
-            <Route path='notification' element={<div></div>} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
