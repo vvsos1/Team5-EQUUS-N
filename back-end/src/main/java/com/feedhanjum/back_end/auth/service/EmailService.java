13bb821eda5fc198e7296b9504a2a3c5417b99d1
vvsos1 <vvsos10@naver.com>
2025-02-07 15:19:29 +0900

    [FD-40] chore: 이메일 로그 추가

--- back-end/src/main/java/com/feedhanjum/back_end/auth/service/EmailService.java
+++ back-end/src/main/java/com/feedhanjum/back_end/auth/service/EmailService.java
@@ -1,11 +1,13 @@
 package com.feedhanjum.back_end.auth.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class EmailService {
@@ -13,6 +15,7 @@ public class EmailService {
 
     @Async
     public void sendMail(String to, String title, String content) {
+        log.info("email to: {}, title: {}, content:{}", to, title, content);
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(title);
