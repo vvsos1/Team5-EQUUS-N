830d8b0076551139f72c9f4e4cac010bae9be3ec
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-08 23:25:26 +0900

    [FD-305] docs: 일정 내 할 일 응답 DTO 스키마 작성

--- back-end/src/main/java/com/feedhanjum/back_end/schedule/service/dto/ScheduleMemberNestedDto.java
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/service/dto/ScheduleMemberNestedDto.java
@@ -1,20 +1,26 @@
 package com.feedhanjum.back_end.schedule.service.dto;
 
 import com.feedhanjum.back_end.schedule.repository.dto.ScheduleProjectionDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Schema(description = "스케줄 멤버 및 할 일 목록 데이터를 포함하는 DTO")
 public class ScheduleMemberNestedDto{
-    private Long scheduleMemberId;
+
+    @Schema(description = "사용자의 ID")
     private Long memberId;
+    
+    @Schema(description = "사용자의 활동 이름")
     private String memberName;
+    
+    @Schema(description = "할 일 목록")
     private List<String> todoList = new ArrayList<>();
 
     public ScheduleMemberNestedDto(ScheduleProjectionDto scheduleProjectionDto){
-        scheduleMemberId = scheduleProjectionDto.getScheduleMemberId();
         memberId = scheduleProjectionDto.getMemberId();
         memberName = scheduleProjectionDto.getMemberName();
     }
