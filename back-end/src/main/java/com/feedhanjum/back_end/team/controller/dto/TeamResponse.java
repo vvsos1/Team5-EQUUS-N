580856285869591510523872fafd85e0926e19d8
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-08 23:27:03 +0900

    [FD-305] feat: 팀 정보 응답 DTO 스키마 작성

--- back-end/src/main/java/com/feedhanjum/back_end/team/controller/dto/TeamResponse.java
+++ back-end/src/main/java/com/feedhanjum/back_end/team/controller/dto/TeamResponse.java
@@ -3,15 +3,27 @@ package com.feedhanjum.back_end.team.controller.dto;
 import com.feedhanjum.back_end.feedback.domain.FeedbackType;
 import com.feedhanjum.back_end.member.controller.dto.MemberDto;
 import com.feedhanjum.back_end.team.domain.Team;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
 public record TeamResponse(
+        @Schema(description = "팀 고유 ID")
         Long id,
+        
+        @Schema(description = "팀 이름")
         String name,
+        
+        @Schema(description = "팀 시작 날짜")
         LocalDate startDate,
+        
+        @Schema(description = "팀 종료 날짜")
         LocalDate endDate,
+        
+        @Schema(description = "피드백 유형")
         FeedbackType feedbackType,
+        
+        @Schema(description = "팀장 정보")
         MemberDto leader
 ) {
     public TeamResponse(Team team) {
