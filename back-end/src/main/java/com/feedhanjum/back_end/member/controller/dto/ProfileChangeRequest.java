404c97c76f26ab72413d119a1f71b441d8069021
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-08 23:24:52 +0900

    [FD-305] docs: 프로필 변경 요청 DTO 스키마 작성

--- back-end/src/main/java/com/feedhanjum/back_end/member/controller/dto/ProfileChangeRequest.java
+++ back-end/src/main/java/com/feedhanjum/back_end/member/controller/dto/ProfileChangeRequest.java
@@ -1,13 +1,17 @@
 package com.feedhanjum.back_end.member.controller.dto;
 
 import com.feedhanjum.back_end.member.domain.ProfileImage;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ProfileChangeRequest (
+        @Schema(description = "변경할 활동명을 입력합니다. 활동명은 1자 이상, 20자 이하여야 합니다.")
         @NotBlank(message = "활동명을 입력해주세요.")
         @Size(min = 1, max = 20, message = "활동명은 1자 이상, 20자 이하여야 합니다.")
         String name,
+        
+        @Schema(description = "변경할 프로필 이미지를 입력합니다.")
         ProfileImage profileImage
 ){
 }
