a0ece175f3f388b58a3fb13d673289b9e3c4445d
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-08 23:25:31 +0900

    [FD-305] docs: 일정 응답 DTO 스키마 작성

--- back-end/src/main/java/com/feedhanjum/back_end/schedule/service/dto/ScheduleNestedDto.java
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/service/dto/ScheduleNestedDto.java
@@ -1,6 +1,7 @@
 package com.feedhanjum.back_end.schedule.service.dto;
 
 import com.feedhanjum.back_end.schedule.repository.dto.ScheduleProjectionDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,12 +10,21 @@ import java.util.List;
 
 @Data
 public class ScheduleNestedDto{
+    @Schema(description = "일정 ID")
     private Long scheduleId;
+    @Schema(description = "일정 이름")
     private String scheduleName;
+    @Schema(description = "일정 시작 시간")
     private LocalDateTime startTime;
+    @Schema(description = "일정 종료 시간")
     private LocalDateTime endTime;
+    @Schema(description = "팀 ID")
     private Long teamId;
+    @Schema(description = "팀 이름")
     private String teamName;
+    @Schema(description = "리더 ID")
+    private Long leaderId;
+    @Schema(description = "일정에 포함된 팀원 별 할 일 목록")
     private List<ScheduleMemberNestedDto> scheduleMemberNestedDtoList = new ArrayList<>();
 
     public ScheduleNestedDto(ScheduleProjectionDto scheduleProjectionDto) {
@@ -24,6 +34,7 @@ public class ScheduleNestedDto{
         endTime = scheduleProjectionDto.getEndTime();
         teamId = scheduleProjectionDto.getTeamId();
         teamName = scheduleProjectionDto.getTeamName();
+        leaderId = scheduleProjectionDto.getLeaderId();
     }
 
     public void addScheduleMemberNestedDto(ScheduleMemberNestedDto scheduleMemberNestedDto){
