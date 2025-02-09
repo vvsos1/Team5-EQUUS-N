3f6a20361ded39149a3d2a020849bc7a83691a47
Se Jun Lim <lsj1137@naver.com>
2025-02-09 14:35:36 +0900

    [FD-216] feat: TeamElement 눌러서 팀원목록 들어가도록 설정. 팀원 목록화면 구성

--- front-end/src/pages/teamspace/components/MemberElement.jsx
+++ front-end/src/pages/teamspace/components/MemberElement.jsx
@@ -12,28 +12,30 @@ import Tag, { TagType } from '../../../components/Tag';
  * @param {boolean} props.isLeader - 팀장 여부
  * @returns {JSX.Element} - 팀원 요소 컴포넌트
  */
-export default function MemberElement({ member, isLeader }) {
+export default function MemberElement({ member, leaderId, iamLeader }) {
   return (
     <li
       className={`rounded-400 flex h-fit w-full items-center bg-gray-800 px-5 py-4`}
     >
       <div className='h-11 w-11'>
         <ProfileImage
-          iconName={`@animals/${member.iconName}`}
-          color={member.color}
+          iconName={`@animals/${member.profileImage?.image}`}
+          color={
+            member.profileImage?.backgroundColor ?? 'var(--color-gray-200)'
+          }
         />
       </div>
       <div className='w-3' />
-      <div className='flex flex-1 flex-col gap-1'>
+      <div className='flex flex-1 flex-col gap-2'>
         <div className='flex items-center gap-3'>
           <p className='subtitle-1 text-gray-100'>{member.name}</p>
-          {isLeader ?
+          {member.id === leaderId ?
             <Tag type={TagType.TEAM_LEADER} />
           : null}
         </div>
         <p className='caption-1 text-gray-300'>{member.email}</p>
       </div>
-      {!isLeader ?
+      {iamLeader && member.id !== leaderId ?
         <div className='flex gap-2'>
           <div
             className='flex h-9 w-9 cursor-pointer items-center justify-center rounded-full bg-gray-600 p-1.5'
@@ -52,38 +54,3 @@ export default function MemberElement({ member, isLeader }) {
     </li>
   );
 }
-
-// const memberList = [
-//   {
-//     name: '한준호',
-//     email: 'john.doe@example.com',
-//     iconName: 'panda',
-//     color: '#62BFCA',
-//     isLeader: true,
-//   },
-//   {
-//     name: '박명규',
-//     email: 'john.doe@example.com',
-//     iconName: 'penguin',
-//     color: '#F6D480',
-//     isLeader: false,
-//   },
-//   {
-//     name: '임세준',
-//     email: 'john.doe@example.com',
-//     iconName: 'rooster',
-//     color: '#F17858',
-//     isLeader: false,
-//   },
-//   {
-//     name: '백현식',
-//     email: 'john.doe@example.com',
-//     iconName: 'lion',
-//     color: '#58AECB',
-//     isLeader: false,
-//   },
-// ];
-
-// {memberList.map((member, index) => (
-//     <MemberElement key={index} member={member} isLeader={member.isLeader} />
-//   ))}
