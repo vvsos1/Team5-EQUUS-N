2520f1d7247861e3d492fffb87d32a6db9970abc
vvsos1 <vvsos10@naver.com>
2025-02-09 18:30:58 +0900

    [FD-86] feat: WebPush 설정 추가 및 메일 설정 dev 프로파일로 이동

---
+++ back-end/src/main/java/com/feedhanjum/back_end/notification/config/WebPushConfig.java
@@ -0,0 +1,24 @@
+package com.feedhanjum.back_end.notification.config;
+
+import lombok.extern.slf4j.Slf4j;
+import nl.martijndwars.webpush.PushAsyncService;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.GeneralSecurityException;
+import java.security.Security;
+
+@Slf4j
+@Configuration
+public class WebPushConfig {
+
+    @Bean
+    public PushAsyncService pushAsyncService(WebPushProperty webPushProperty) throws GeneralSecurityException {
+        log.info("Initializing PushAsyncService. public key: {}, private key: {}, subject: {}",
+                webPushProperty.getVapid().getPublicKey(), webPushProperty.getVapid().getPrivateKey(), webPushProperty.getSubject());
+        Security.addProvider(new BouncyCastleProvider());
+        return new PushAsyncService(webPushProperty.getVapid().getPublicKey(), webPushProperty.getVapid().getPrivateKey(), webPushProperty.getSubject());
+    }
+
+}
