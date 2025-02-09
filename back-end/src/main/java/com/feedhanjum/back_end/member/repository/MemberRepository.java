c1e990d2e8c4239cadaa048aec1b5bba2814543a
GoGradually <chkun3109@gmail.com>
2025-02-09 22:27:24 +0900

    [FD-103] feat: 피드백 선호 정보를 fetch join 하는 별도의 repository 메소드 추가

--- back-end/src/main/java/com/feedhanjum/back_end/member/repository/MemberRepository.java
+++ back-end/src/main/java/com/feedhanjum/back_end/member/repository/MemberRepository.java
@@ -2,6 +2,11 @@ package com.feedhanjum.back_end.member.repository;
 
 import com.feedhanjum.back_end.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    @Query("select m from Member m join fetch m.feedbackPreferences fp")
+    Optional<Member> findMemberAndFeedbackPreferenceById(Long id);
 }
