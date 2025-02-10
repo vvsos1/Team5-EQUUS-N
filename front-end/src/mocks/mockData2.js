fdcad314c836de8ba78fc92c7dfdaaa03df93a8b
lsj1137 <lsj1137@naver.com>
2025-02-10 18:38:01 +0900

    [FD-207] chore: mock Data 추가

--- front-end/src/mocks/mockData2.js
+++ front-end/src/mocks/mockData2.js
@@ -67,126 +67,162 @@ export const teams2 = [
 
 export const schedules2 = [
   {
-    teamId: 1,
+    scheduleId: 1,
+    scheduleName: '스케줄 내용1',
+    startTime: '2025-02-09T22:53:04.033Z',
+    endTime: '2025-02-10T06:53:04.033Z',
+    teamId: 1234151,
     teamName: '소프티어 5조',
-    scheduleInfo: {
-      startTime: '2025-02-05T17:00:00.000Z',
-      endTime: '2025-02-05T18:00:00.000Z',
-      scheduleName: '스케줄 내용1',
-    },
-    todos: [
-      // {
-      //   memberId: 1,
-      //   name: '임세준',
-      //   task: ['데스크 리서치 하기', '설문지 작성하기'],
-      // },
-      {
-        memberId: 2,
-        name: '백현식',
-        task: ['레퍼런스 리서치 하기', '설문지 배포하기'],
-      },
-      {
-        memberId: 3,
-        name: '한준호',
-        task: [],
-      },
-      {
-        memberId: 4,
-        name: '박명규',
-        task: ['프레젠테이션 연습하기', '설문지 작성하기'],
-      },
-    ],
-  },
-  {
-    teamId: 2,
-    teamName: '에쿠스 N',
-    scheduleInfo: {
-      startTime: '2025-02-04T17:00:00.000Z',
-      endTime: '2025-02-04T18:00:00.000Z',
-      scheduleName: '스케줄 내용2',
-    },
-    todos: [
+    leaderId: 52345513,
+    ownerId: 9007199254740991,
+    scheduleMemberNestedDtoList: [
       {
         memberId: 1,
-        name: '임세준',
-        task: ['데스크 리서치 하기', '설문지 작성하기'],
+        memberName: '임세준',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
       },
       {
         memberId: 2,
-        name: '백현식',
-        task: ['레퍼런스 리서치 하기', '설문지 배포하기'],
+        memberName: '백현식',
+        todoList: ['레퍼런스 리서치 하기', '설문지 배포하기'],
       },
       {
         memberId: 3,
-        name: '한준호',
-        task: [],
+        memberName: '한준호',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
       },
       {
         memberId: 4,
-        name: '박명규',
-        task: ['프레젠테이션 연습하기', '설문지 작성하기'],
+        memberName: '박명규',
+        todoList: ['프레젠테이션 연습하기', '설문지 작성하기'],
       },
     ],
   },
   {
-    teamId: 3,
+    scheduleId: 5,
+    scheduleName: '스케줄 내용1',
+    startTime: '2025-02-10T06:53:04.033Z',
+    endTime: '2025-02-11T06:53:04.033Z',
+    teamId: 1234151,
+    teamName: '소프티어 5조',
+    leaderId: 52345513,
+    ownerId: 9007199254740991,
+    scheduleMemberNestedDtoList: [
+      {
+        memberId: 1,
+        memberName: '임세준',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 2,
+        memberName: '백현식',
+        todoList: ['레퍼런스 리서치 하기', '설문지 배포하기'],
+      },
+      {
+        memberId: 3,
+        memberName: '한준호',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 4,
+        memberName: '박명규',
+        todoList: ['프레젠테이션 연습하기', '설문지 작성하기'],
+      },
+    ],
+  },
+  {
+    scheduleId: 2,
+    scheduleName: '스케줄 내용1',
+    startTime: '2025-02-10T06:53:04.033Z',
+    endTime: '2025-02-11T06:53:04.033Z',
+    teamId: 1234151,
+    teamName: '소프티어 5조',
+    leaderId: 52345513,
+    ownerId: 9007199254740991,
+    scheduleMemberNestedDtoList: [
+      {
+        memberId: 1,
+        memberName: '임세준',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 2,
+        memberName: '백현식',
+        todoList: ['레퍼런스 리서치 하기', '설문지 배포하기'],
+      },
+      {
+        memberId: 3,
+        memberName: '한준호',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 4,
+        memberName: '박명규',
+        todoList: ['프레젠테이션 연습하기', '설문지 작성하기'],
+      },
+    ],
+  },
+  {
+    scheduleId: 3,
+    scheduleName: '스케줄 내용1',
+    startTime: '2025-02-12T06:53:04.033Z',
+    endTime: '2025-02-13T06:53:04.033Z',
+    teamId: 1234151,
+    teamName: '소프티어 5조',
+    leaderId: 52345513,
+    ownerId: 9007199254740991,
+    scheduleMemberNestedDtoList: [
+      {
+        memberId: 1,
+        memberName: '임세준',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 2,
+        memberName: '백현식',
+        todoList: ['레퍼런스 리서치 하기', '설문지 배포하기'],
+      },
+      {
+        memberId: 3,
+        memberName: '한준호',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
+      },
+      {
+        memberId: 4,
+        memberName: '박명규',
+        todoList: ['프레젠테이션 연습하기', '설문지 작성하기'],
+      },
+    ],
+  },
+  {
+    scheduleId: 4,
+    scheduleName: '스케줄 내용1',
+    startTime: '2025-02-12T06:53:04.033Z',
+    endTime: '2025-02-13T06:53:04.033Z',
+    teamId: 4321,
     teamName: '협곡의 전사들',
-    scheduleInfo: {
-      startTime: '2025-02-07T17:00:00.000Z',
-      endTime: '2025-02-08T18:00:00.000Z',
-      scheduleName: '스케줄 내용3',
-    },
-    todos: [
+    leaderId: 52345513,
+    ownerId: 9007199254740991,
+    scheduleMemberNestedDtoList: [
       {
         memberId: 1,
-        name: '임세준',
-        task: ['데스크 리서치 하기', '설문지 작성하기'],
+        memberName: '임세준',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
       },
       {
         memberId: 2,
-        name: '백현식',
-        task: ['레퍼런스 리서치 하기', '설문지 배포하기'],
+        memberName: '백현식',
+        todoList: ['레퍼런스 리서치 하기', '설문지 배포하기'],
       },
       {
         memberId: 3,
-        name: '한준호',
-        task: [],
+        memberName: '한준호',
+        todoList: ['데스크 리서치 하기', '설문지 작성하기'],
       },
       {
         memberId: 4,
-        name: '박명규',
-        task: ['프레젠테이션 연습하기', '설문지 작성하기'],
-      },
-    ],
-  },
-  {
-    teamId: 4,
-    teamName: '한박백임',
-    scheduleInfo: {
-      startTime: '2025-02-06T17:00:00.000Z',
-      endTime: '2025-02-06T19:00:00.000Z',
-      scheduleName: '스케줄 내용4',
-    },
-    todos: [
-      {
-        memberId: 1,
-        name: '임세준',
-        task: ['데스크 리서치 하기', '설문지 작성하기'],
-      },
-      {
-        memberId: 2,
-        name: '백현식',
-        task: ['레퍼런스 리서치 하기', '설문지 배포하기'],
-      },
-      {
-        memberId: 3,
-        name: '한준호',
-        task: [],
-      },
-      {
-        memberId: 4,
-        name: '박명규',
-        task: ['프레젠테이션 연습하기', '설문지 작성하기'],
+        memberName: '박명규',
+        todoList: ['프레젠테이션 연습하기', '설문지 작성하기'],
       },
     ],
   },
