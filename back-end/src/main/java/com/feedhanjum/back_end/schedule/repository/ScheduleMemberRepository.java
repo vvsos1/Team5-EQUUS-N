74a6ccde9278f31b4c4d6af460669e595ac8d37f
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-11 17:47:40 +0900

    [FD-263] feat: 사용자를 기존에 등록된 일정에서 제거하는 리포지토리 쿼리 구현

--- back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleMemberRepository.java
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleMemberRepository.java
@@ -2,10 +2,17 @@ package com.feedhanjum.back_end.schedule.repository;
 
 import com.feedhanjum.back_end.schedule.domain.ScheduleMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ScheduleMemberRepository extends JpaRepository<ScheduleMember, Long> {
 
     Optional<ScheduleMember> findByMemberIdAndScheduleId(Long memberId, Long scheduleId);
+
+    @Modifying
+    @Query("delete from ScheduleMember sm where sm.member.id = :memberId and sm.schedule.team.id = :teamId and sm.schedule.endTime > :now")
+    void deleteScheduleMembersByMemberIdAndTeamIdAfterNow(Long memberId, Long teamId, LocalDateTime now);
 }
