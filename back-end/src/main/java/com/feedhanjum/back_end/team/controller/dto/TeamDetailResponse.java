4cbbf2ae1a9400e69abd6b5e34be0eb5c4851a21
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-11 14:16:11 +0900

    [FD-320] docs: 팀의 상세 정보 중, 일정의 시작/종료 시간에 대한 설명 추가

--- back-end/src/main/java/com/feedhanjum/back_end/team/controller/dto/TeamDetailResponse.java
+++ back-end/src/main/java/com/feedhanjum/back_end/team/controller/dto/TeamDetailResponse.java
@@ -12,10 +12,10 @@ public class TeamDetailResponse {
     @Schema(description = "팀 응답 정보")
     private TeamResponse teamResponse;
 
-    @Schema(description = "일정의 가장 빠른 시작 시간")
+    @Schema(description = "일정의 가장 빠른 시작 시간. null일 경우, 일정이 존재하지 않음")
     private LocalDateTime earliestScheduleStartTime;
 
-    @Schema(description = "일정의 가장 늦은 종료 시간")
+    @Schema(description = "일정의 가장 늦은 종료 시간. null일 경우, 일정이 존재하지 않음")
     private LocalDateTime latestScheduleEndTime;
 
     @Schema(description = "팀 멤버 목록")
