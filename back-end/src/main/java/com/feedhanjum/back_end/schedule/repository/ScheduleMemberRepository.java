a6c362699dc6b9c7d98b732e5624d7c25107dda2
Junho Han <62929862+GoGradually@users.noreply.github.com>
2025-02-11 22:39:26 +0900

    [FD-323] feat: 일정-사용자 연관관계 삭제 쿼리 시 코드 안정성을 위해 1차 캐시 비우기 작업 진행

--- back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleMemberRepository.java
+++ back-end/src/main/java/com/feedhanjum/back_end/schedule/repository/ScheduleMemberRepository.java
@@ -4,6 +4,7 @@ import com.feedhanjum.back_end.schedule.domain.ScheduleMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -12,7 +13,8 @@ public interface ScheduleMemberRepository extends JpaRepository<ScheduleMember, 
 
     Optional<ScheduleMember> findByMemberIdAndScheduleId(Long memberId, Long scheduleId);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
+    @Transactional
     @Query("delete from ScheduleMember sm where sm.member.id = :memberId and sm.schedule.team.id = :teamId and sm.schedule.endTime > :now")
     void deleteScheduleMembersByMemberIdAndTeamIdAfterNow(Long memberId, Long teamId, LocalDateTime now);
 }
