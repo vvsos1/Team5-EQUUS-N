201e412fa437d7cbcd5d37e84d3098d2fdbd7192
vvsos1 <vvsos10@naver.com>
2025-02-15 14:55:01 +0900

    [FD-351] feat: WebPushService 구독 중복 방지 로직 추가

--- back-end/src/main/java/com/feedhanjum/back_end/notification/repository/WebPushSubscriptionRepository.java
+++ back-end/src/main/java/com/feedhanjum/back_end/notification/repository/WebPushSubscriptionRepository.java
@@ -5,7 +5,10 @@ import com.feedhanjum.back_end.notification.domain.WebPushSubscription;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WebPushSubscriptionRepository extends JpaRepository<WebPushSubscription, Long> {
     List<WebPushSubscription> findAllBySubscriber(Member subscriber);
+
+    Optional<WebPushSubscription> findBySubscription_Endpoint(String subscriptionEndpoint);
 }
