99415b722808248e118418758719af624c008d27
vvsos1 <vvsos10@naver.com>
2025-03-06 16:39:21 +0900

    test: 테스트용 PersistenceAdapterTest 어노테이션 추가

---
+++ back-end/src/test/java/com/feedhanjum/back_end/test/config/QuerydslTestConfiguration.java
@@ -0,0 +1,14 @@
+package com.feedhanjum.back_end.test.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration
+public class QuerydslTestConfiguration {
+    @Bean
+    public JPAQueryFactory queryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
+}
